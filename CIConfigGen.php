88a16f9cd59cd444b49a425cdb700f6b5b03c573
Kerrial Newham <kerrialbeckettnewham@gmail.com>
2019-12-21 14:32:00 +0100

    initial commit

---
+++ CIConfigGen.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once  __DIR__ . '/vendor/autoload.php';
+
+use Symfony\Component\Console\Application;
+
+$application = new Application();
+$application->run();
---
+++ src/WhatTime.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace CIConfigGen;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class WhatTime extends Command {
+
+    protected function configure()
+    {
+        $this->setName('gc:WhatTime')->setDescription('Display current time')->setHelp('Print the current time to STDOUT');
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $now = new DateTime();
+        $output->writeln('its now ' . $now->format('g:i a'));
+    }
+
+}
+
