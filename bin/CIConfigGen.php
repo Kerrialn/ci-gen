04cd306c97e786f1f7aabdc75f3f85325cd2d462
TomasVotruba <tomas.vot@gmail.com>
2019-12-21 16:16:41 +0100

    add command

--- bin/CIConfigGen.php
+++ bin/CIConfigGen.php
@@ -3,7 +3,9 @@
 
 require_once  __DIR__ . '/../vendor/autoload.php';
 
+use CIConfigGen\WhatTimeCommand;
 use Symfony\Component\Console\Application;
 
 $application = new Application();
+$application->add(new WhatTimeCommand());
 $application->run();
