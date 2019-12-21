d2acb860078d1456c547f0da1dea250cfc6ca151
TomasVotruba <tomas.vot@gmail.com>
2019-12-21 16:03:39 +0100

    fix path to autoload

--- bin/CIConfigGen.php
+++ bin/CIConfigGen.php
@@ -1,8 +1,6 @@
 <?php
 
-namespace CIConfigGen;
-
-require_once  __DIR__ . '/vendor/autoload.php';
+require_once  __DIR__ . '/../vendor/autoload.php';
 
 use Symfony\Component\Console\Application;
 
