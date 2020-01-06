a3f0b81e54641c6984e74bbb7d0263c13a90af19
Kerrial Newham <kerrialbeckettnewham@gmail.com>
2020-01-06 12:59:18 +0100

    fix Gitlab generator

--- src/Generator/GitlabGenerator.php
+++ src/Generator/GitlabGenerator.php
@@ -4,20 +4,26 @@ declare(strict_types=1);
 
 namespace CIConfigGen\Generator;
 
+use CIConfigGen\Composer\VersionResolver;
 use CIConfigGen\Contract\GeneratorInterface;
+use CIConfigGen\ScriptFactory\ECSFactory;
 use CIConfigGen\ScriptFactory\PHPUnitScriptFactory;
 use CIConfigGen\ValueObject\CiService;
 
-final class GitlabGenerator implements GeneratorInterface
-{
+final class GitlabGenerator implements GeneratorInterface {
+
     /**
      * @var PHPUnitScriptFactory
      */
     private $phpUnitScriptFactory;
+    private $versionResolver;
+    private $ecsFactory;
 
-    public function __construct(PHPUnitScriptFactory $phpUnitScriptFactory)
+    public function __construct(PHPUnitScriptFactory $phpUnitScriptFactory, VersionResolver $versionResolver, ECSFactory $ecsFactory)
     {
         $this->phpUnitScriptFactory = $phpUnitScriptFactory;
+        $this->versionResolver = $versionResolver;
+        $this->ecsFactory = $ecsFactory;
     }
 
     public function isMatch(string $ciService): bool
@@ -27,17 +33,50 @@ final class GitlabGenerator implements GeneratorInterface
 
     public function generate(array $composerJson): array
     {
-        return [
-            'name' => CiService::GITLAB_CI,
-            'language' => 'PHP',
-            'stages' => ['test'],
+        $yaml = [];
 
-            'job' => [
+        $phpVersions = $this->versionResolver->resolvePhpVersions($composerJson);
+        $phpunitJob = $this->phpUnitScriptFactory->create();
+        $ecsJob = $this->ecsFactory->create($composerJson);
+
+        if ($composerJson)
+        {
+            $yaml['stages'][] = 'install';
+        }
+        if ($phpunitJob || $ecsJob)
+        {
+            $yaml['stages'][] = 'test';
+        }
+        if ($this->versionResolver->getMinimalVersion($composerJson))
+        {
+            $yaml['stages'][] = 'build';
+        }
+
+        if ($composerJson)
+        {
+            $yaml['install'] = [
+                'stage' => 'install',
+                'script' => ['composer verify', 'composer install'],
+            ];
+        }
+
+        if ($phpunitJob)
+        {
+            $yaml['phpunit_jobs'] = [
                 'stage' => 'test',
-                'php' => $composerJson['require']['php'],
-                'script' => $this->phpUnitScriptFactory->create(),
-            ],
-        ];
+                'script' => $phpunitJob,
+            ];
+        }
+
+        if ($ecsJob)
+        {
+            $yaml['ecs_jobs'] = [
+                'stage' => 'test',
+                'script' => $ecsJob,
+            ];
+        }
+
+        return $yaml;
     }
 
     public function getFilename(): string
