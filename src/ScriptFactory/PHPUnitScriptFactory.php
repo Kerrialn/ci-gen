35543e16f3b907ff38a584d90072d8866e103fa5
Kerrial Newham <kerrialbeckettnewham@gmail.com>
2020-02-15 12:28:22 +0100

    fix github actions

--- src/ScriptFactory/PHPUnitScriptFactory.php
+++ src/ScriptFactory/PHPUnitScriptFactory.php
@@ -6,7 +6,7 @@ namespace CIConfigGen\ScriptFactory;
 
 use CIConfigGen\Xml\XmlReader;
 
-final class PHPUnitScriptFactory
+final class  PHPUnitScriptFactory
 {
     /**
      * @var XmlReader
@@ -31,7 +31,7 @@ final class PHPUnitScriptFactory
 
         if ($array['testsuite']) {
             $testsuiteName = $array['testsuite']['@attributes']['name'];
-            $tests[] = sprintf('vendor/bin/phpunit --testsuite %s', $testsuiteName);
+            $tests[] = sprintf('vendor/bin/phpunit %s', $testsuiteName);
         } else {
             $tests[] = 'skip';
         }
