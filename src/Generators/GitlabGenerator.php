a7fce1486834e6c70bfe46a7577228776d759e48
Kerrial Newham <kerrialbeckettnewham@gmail.com>
2020-03-23 21:41:35 +0100

    test gitlab ci 4

--- src/Generators/GitlabGenerator.php
+++ src/Generators/GitlabGenerator.php
@@ -24,6 +24,7 @@ final class GitlabGenerator implements GeneratorInterface {
     {
         $output = [
             'image' => 'php:latest',
+            'before_script' => ['php composer install'],
             'stages' => [
                 'test'
             ],
