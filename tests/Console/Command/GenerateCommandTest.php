b64aaaa206dc85f0a872538be66a7d2e7e6eabc0
Kerrial Newham <kerrialbeckettnewham@gmail.com>
2020-03-25 20:41:38 +0100

    update tests

--- tests/Console/Command/GenerateCommandTest.php
+++ tests/Console/Command/GenerateCommandTest.php
@@ -4,38 +4,25 @@ declare(strict_types=1);
 
 namespace App\Tests\Console\Command;
 
-use Symfony\Bundle\FrameworkBundle\Console\Application;
-use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
-use Symfony\Component\Console\Tester\CommandTester;
-use Symfony\Component\Dotenv\Dotenv;
 
-class GenerateCommandTest extends KernelTestCase
+use PHPUnit\Framework\TestCase;
+
+class GenerateCommandTest extends TestCase
 {
-    public function testExecute()
+    /**
+     * @dataProvider provider
+     * @param $data
+     */
+    public function testMethod($data)
     {
-        $dotenv = new Dotenv();
-        $dotenv->load(__DIR__ . '/.env.test');
+        $this->assertTrue($data);
+    }
 
-        $kernel = self::bootKernel();
-        $application = new Application($kernel);
-
-        $executeSalesCommand = self::$kernel->getContainer()->get(
-            'console.command.public_alias.App\Command\ExecuteSalesTaskCommand'
-        );
-
-        $application->add($executeSalesCommand);
-
-        $command = $application->find('app:execute-sales-task');
-        $commandTester = new CommandTester($command);
-        $commandTester->execute(
-            [
-                'command' => $command->getName(),
-            ]
-        );
-
-        // the output of the command in the console
-        $output = $commandTester->getDisplay();
-
-        // do your asserting stuff
+    public function provider()
+    {
+        return [
+            'my named data' => [true],
+            'my data' => [true],
+        ];
     }
 }
