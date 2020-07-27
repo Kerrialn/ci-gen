450ee2cf0b86eca46a5a99ddd73fd08ab12ad0fb
Kerrial Newham <kerrialbeckettnewham@gmail.com>
2020-07-27 12:45:43 +0200

    update

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symplify\ParameterNameGuard\ParameterNameGuardBundle::class => ['dev' => true, 'test' => true],
+    Symplify\ConsoleColorDiff\ConsoleColorDiffBundle::class => ['dev' => true, 'test' => true],
 ];
